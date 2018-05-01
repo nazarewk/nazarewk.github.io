2746b87e830999ee89af65751c47959eaf197ec0
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2018-05-01 09:42:21 +0200

    Update archiso-ssh.sh

--- archiso-ssh.sh
+++ archiso-ssh.sh
@@ -3,3 +3,5 @@
 [ -d "$HOME/.ssh" ] || mkdir -m 700 $HOME/.ssh
 curl https://nazarewk.github.io/nazarewk.pub > "$HOME/.ssh/authorized_keys"
 systemctl start sshd.service
+
+ip a | grep -o 'inet [0-9]\+'
