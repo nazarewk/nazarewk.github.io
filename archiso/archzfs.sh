df3567d4cccc1de1ffb775b41829caf451a0eff5
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2018-05-01 10:05:07 +0200

    Update archzfs.sh

--- archiso/archzfs.sh
+++ archiso/archzfs.sh
@@ -3,6 +3,6 @@ pacman-key --recv-keys 5E1ABF240EE7A126
 pacman-key --lsign-key 5E1ABF240EE7A126
 cat <<EOF >> /etc/pacman.conf
 [archzfs]
-Server = http://archzfs.com/archzfs/x86_64
+Server = http://archzfs.com/\$repo/x86_64
 EOF
 pacman -Syy
